e163f77888e2184374df63f3dcd458e082fccf87
pengge <tela2020@foxmail.com>
2024-08-14 14:11:04 +0800

    Update RpcQuickAction.java

--- src/main/java/cn/xiaoheiban/action/RpcQuickAction.java
+++ src/main/java/cn/xiaoheiban/action/RpcQuickAction.java
@@ -7,6 +7,7 @@ import cn.xiaoheiban.ui.FileChooseDialog;
 import cn.xiaoheiban.util.Exec;
 import cn.xiaoheiban.util.FileReload;
 import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.progress.ProgressIndicator;
@@ -39,7 +40,10 @@ public class RpcQuickAction extends AnAction {
             e.getPresentation().setEnabledAndVisible(false);
         }
     }
-
+    @Override
+    public @NotNull ActionUpdateThread getActionUpdateThread() {
+        return ActionUpdateThread.BGT;
+    }
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         VirtualFile file = e.getData(PlatformDataKeys.VIRTUAL_FILE);
